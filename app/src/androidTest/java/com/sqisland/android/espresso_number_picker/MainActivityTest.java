ece657d0e4acf9fc1787ec41babe00ee5d8bac8e
Chiu-Ki Chan <chiuki@gmail.com>
2015-01-07 20:53:17 -0700

    Upgrade to Espresso 2.0

--- app/src/androidTest/java/com/sqisland/android/espresso_number_picker/MainActivityTest.java
+++ app/src/androidTest/java/com/sqisland/android/espresso_number_picker/MainActivityTest.java
@@ -3,10 +3,10 @@ package com.sqisland.android.espresso_number_picker;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.NumberPicker;
 
-import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
-import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isAssignableFrom;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
   public MainActivityTest() {
